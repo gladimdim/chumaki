a231f8d6670e8e1743449ca16a86bb2e92eeaa0c
Dmytro Gladkyi <gladimdim@gmail.com>
2021-04-05 17:06:32 +0300

    Initial commit.

---
+++ android/app/src/main/kotlin/com/example/chumaki/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chumaki
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:chumaki/views/main_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainView(),
+    );
+  }
+}
