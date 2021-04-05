b34e5b32d2c9465a1db4613562e94a13928c85f8
Dmytro Gladkyi <gladimdim@gmail.com>
2021-04-05 22:38:58 +0300

    Can draw city spots on the stack widget.

--- lib/views/main_view.dart
+++ lib/views/main_view.dart
@@ -1,8 +1,57 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class MainView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: [
+        Container(
+          color: Colors.white,
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+
+        ),
+        ...cities.map((city) {
+          return  Positioned(
+            top: city.y,
+            left: city.x,
+            child: GestureDetector(
+              onTap: () {
+                print("pressed circle");
+              },
+              child: ClipOval(
+                child: SizedBox(
+                  width: 60,
+                  height: 60,
+                  child: Container(
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+            ),
+          );
+        }),
+
+      ],
+    );
+  }
+}
+
+List<Point<double>> cities = [Point(100,200), Point(300, 50), Point(200, 75)];
+
+class CurvePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+        ..color = Colors.red
+        ..style = PaintingStyle.fill;
+    canvas.drawCircle(Offset(30, 30), 30, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
