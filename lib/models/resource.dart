32c0ab531a9fb01c1e0e60445f3b927e2fde613f
Dmytro Gladkyi <gladimdim@gmail.com>
2021-04-12 19:12:22 +0300

    Added resource images.
    Added Resource class.

---
+++ lib/models/resource.dart
@@ -0,0 +1,93 @@
+class Resource {
+  final String localizedKey;
+  int amount;
+  Resource(this.localizedKey, this.amount);
+
+  String get imagePath {
+    return "images/resources/${this.localizedKey}";
+  }
+
+  bool sameType(Resource another) {
+    return another.localizedKey == localizedKey;
+  }
+
+  @override
+  bool operator ==(another) {
+    if (another is! Resource) {
+      return false;
+    }
+    return (another.localizedKey == localizedKey && another.amount == amount);
+  }
+
+  @override
+  int get hashCode {
+    return localizedKey.hashCode;
+  }
+
+}
+
+class Wood extends Resource {
+  Wood(int amount): super("wood", amount);
+}
+
+class Cannon extends Resource {
+  Cannon(int amount): super("cannon", amount);
+}
+
+class Charcoal extends Resource {
+  Charcoal(int amount): super("charcoal", amount);
+}
+
+class Firearm extends Resource {
+  Firearm(int amount): super("firearm", amount);
+}
+
+class Fish extends Resource {
+  Fish(int amount): super("fish", amount);
+}
+
+class Food extends Resource {
+  Food(int amount): super("fdood", amount);
+}
+
+class Fur extends Resource {
+  Fur(int amount): super("fure", amount);
+}
+
+class Grains extends Resource {
+  Grains(int amount): super("grains", amount);
+}
+
+class Horse extends Resource {
+  Horse(int amount): super("horse", amount);
+}
+
+class IronOre extends Resource {
+  IronOre(int amount): super("ironore", amount);
+}
+
+class MetalParts extends Resource {
+  MetalParts(int amount): super("metalparts", amount);
+}
+
+class Money extends Resource {
+  Money(int amount): super("money", amount);
+}
+
+class Planks extends Resource {
+  Planks(int amount): super("planks", amount);
+}
+
+class Powder extends Resource {
+  Powder(int amount): super("powder", amount);
+}
+
+class Stone extends Resource {
+  Stone(int amount): super("stone", amount);
+}
+
+
+
+
+
+
--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,15 +1,36 @@
 import 'package:chumaki/models/city.dart';
 import 'package:chumaki/models/progress_duration.dart';
+import 'package:chumaki/models/resource.dart';
 import 'package:uuid/uuid.dart';
+
 class RouteTask extends ProgressDuration {
   final City from;
   final City to;
   DateTime? endTaskDate;
   final String id = Uuid().v4().toString();
 
+  Set<Resource> stock = Set();
+
+  void addToStock(Resource resource) {
+    var inStock = resourceInStock(resource);
+    if (inStock == null) {
+      stock.add(resource);
+    } else {
+      inStock.amount += resource.amount;
+    }
+  }
+
+  Resource? resourceInStock(Resource res) {
+    try {
+      return stock.firstWhere((inStock) => inStock.sameType(res));
+    } catch (e) {
+      return null;
+    }
+  }
+
   RouteTask(
     this.to,
     this.from, {
     duration = const Duration(seconds: 15),
-  }): super(duration);
+  }) : super(duration);
 }
