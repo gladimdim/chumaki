60def686f868dbd5dffcd48134bc1bff40f2e857
Dmytro Gladkyi <gladimdim@gmail.com>
2021-05-08 19:45:51 +0300

    task duration based on distance.

---
+++ lib/utils/time.dart
@@ -0,0 +1,9 @@
+import 'package:intl/intl.dart';
+
+String readableDuration(Duration duration) {
+  var format = DateFormat("HH:mm:ss");
+  var date = DateTime(
+      DateTime.now().year, DateTime.now().month, DateTime.now().day, 0, 0);
+  date = date.add(duration);
+  return format.format(date);
+}
