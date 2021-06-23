9af56127a4f77e60ac06a1faecf763725151f212
gladimdim <gladimdim@gmail.com>
2021-06-23 15:38:22 +0300

    Added Third Sich Event.

--- lib/i18n/event_localizations.dart
+++ lib/i18n/event_localizations.dart
@@ -3,34 +3,54 @@ import 'package:chumaki/i18n/chumaki_localizations.dart';
 class EventLocalizations extends InternalLocalizations {
   Map<String, Map<String, String>> localizedMap = {
     "en": {
+      // sich
       "buyCannonsTitle": "Raid Budzhaks",
       "buyCannonsText":
           "Cossacks are planning the raid on Budzhak tartars. They require some military stuff. Bring it to them to get a reward.",
       "giveFoodTitle": "Frostie winter",
       "giveFoodText":
           "The cold winter was followed by rainy spring. Cossacks were not able to gather a lot of food. Bring them required resources and they will thank you.",
+      "celebrateElectionsTitle": "Celebration",
+      "celebrateElectionsText":
+          "The cossacks elected new Hetman. To celebrate new era in their life they need some good party.",
+      // Chigirin
       "buyClothsForCossacksTitle": "Cloths for newcomers",
-      "buyClothsForCossacksText": "Chigirin regiment received new registered cossacks. Colonel asks you to buy cloths and wool to prepare them for war.",
+      "buyClothsForCossacksText":
+          "Chigirin regiment received new registered cossacks. Colonel asks you to buy cloths and wool to prepare them for war.",
     },
     "uk": {
+      // sich
       "buyCannonsTitle": "Похід на Буджаків",
       "buyCannonsText":
           "Козакам необхідні гармати для рейду на буджацьких татар. Доставте їм необхідні припаси.",
       "giveFoodTitle": "Холодна зима",
       "giveFoodText":
           "Холодна зима та дощова весна знищила надію на гарний урожай восени. Козаки потребують збіжжя та хліба, вони вам віддячать.",
+      "celebrateElectionsTitle": "Святкування",
+      "celebrateElectionsText":
+          "Козаки вибрали нового кошового. Їм треба відмітити це свято гарною гулянкою.",
+
+      // Chigirin
       "buyClothsForCossacksTitle": "Одягнути новачків",
-      "buyClothsForCossacksText": "В Чигиринский полк записали нових реєстровців. Полковник просить надати йому сукно та тканини, щоб одягнути на війну."
+      "buyClothsForCossacksText":
+          "В Чигиринский полк записали нових реєстровців. Полковник просить надати йому сукно та тканини, щоб одягнути на війну."
     },
     "ru": {
+      // Sich
       "buyCannonsTitle": "Поход на буджаков",
       "buyCannonsText":
           "Козаки планируют сделать рейд на буджацких татар. Доставте им необходимые товары.",
       "giveFoodTitle": "Холодная Зима",
       "giveFoodText":
           "Холодная зима и дождевая весна разрушили все надежды на хороший урожай осенью. Козаки требують запасы продовольствия, взамен они вас вознаградять.",
+      "celebrateElectionsTitle": "Празднование",
+      "celebrateElectionsText":
+          "Козаки выбрали нового кошевого. Им надо это отметить хорошей гулянкой.",
+
+      // Chigirin
       "buyClothsForCossacksTitle": "Одеть новобранцев",
-      "buyClothsForCossacksText": "В Чигиринский полк записали новых реестровых козаков. Полковник попросил вас купить им одежду для служения."
+      "buyClothsForCossacksText":
+          "В Чигиринский полк записали новых реестровых козаков. Полковник попросил вас купить им одежду для служения."
     }
   };
 }
--- lib/models/cities/sich.dart
+++ lib/models/cities/sich.dart
@@ -10,62 +10,57 @@ import 'package:chumaki/models/wagon.dart';
 class Sich extends City {
   Sich()
       : super(
-    point: Point(1200, 900),
-    name: "Січ",
-    localizedKeyName: 'sich',
-    size: 4,
-    unlocked: true,
-    unlocksCities: [Ochakiv()],
-    produces: [Powder(1), Fish(1)],
-    availableEvents: [Event(
-      localizedKey: 'events.giveFood',
-      iconPath: 'images/resources/bread/bread_1.png',
-      requirements: [Grains(10), Bread(10)],
-      payment: Money(100),
-    ),
-      Event(
-        localizedKey: 'events.buyCannons',
-        iconPath: 'images/resources/cannon/cannon_3.png',
-        requirements: [Cannon(3), Horse(10)],
-        payment: Money(200),
-      ),
-    ],
-    stock: Stock(
-      [
-        Powder(1000),
-        Fish(1500),
-        Horse(300),
-        Fur(1500),
-        Salt(2000),
-        Wool(100),
-        Silk(300),
-        Gorilka(450),
-        Wax(3000),
-        Honey(1500),
-        Tobacco(500),
-      ],
-    ),
-    wagons: [
-      // Wagon(
-      //     localizedNameKey: "company.tartar",
-      //     stock: Stock([
-      //       Charcoal(20),
-      //       // IronOre(10),
-      //     ])),
-      // Wagon(
-      //     localizedNameKey: "company.ostapa",
-      //     stock: Stock([
-      //       Powder(20),
-      //       MetalParts(10),
-      //     ])),
-      Wagon(
-        stock: Stock(
-          [
-            Grains(50),
-            Fish(50),
+          point: Point(1200, 900),
+          name: "Січ",
+          localizedKeyName: 'sich',
+          size: 4,
+          unlocked: true,
+          unlocksCities: [Ochakiv()],
+          produces: [Powder(1), Fish(1)],
+          availableEvents: [
+            Event(
+              localizedKey: 'events.giveFood',
+              iconPath: 'images/resources/bread/bread_1.png',
+              requirements: [Grains(10), Bread(10)],
+              payment: Money(200),
+            ),
+            Event(
+              localizedKey: 'events.celebrateElections',
+              iconPath: 'images/resources/gorilka/gorilka.png',
+              requirements: [Gorilka(50), Tobacco(30)],
+              payment: Money(200),
+            ),
+            Event(
+              localizedKey: 'events.buyCannons',
+              iconPath: 'images/resources/cannon/cannon_3.png',
+              requirements: [Cannon(3), Horse(10)],
+              payment: Money(1200),
+            ),
           ],
-        ),
-      ),
-    ],
-  );
+          stock: Stock(
+            [
+              Powder(1000),
+              Fish(1500),
+              Horse(300),
+              Fur(1500),
+              Salt(2000),
+              Wool(100),
+              Silk(300),
+              Gorilka(450),
+              Wax(3000),
+              Honey(1500),
+              Tobacco(500),
+            ],
+          ),
+          wagons: [
+            Wagon(
+              stock: Stock(
+                [
+                  Grains(50),
+                  Fish(50),
+                ],
+              ),
+            ),
+          ],
+        );
 }
