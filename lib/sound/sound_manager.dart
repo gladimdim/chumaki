dd761c52e4befb4690a8238f1b52b4c6c34226fb
gladimdim <gladimdim@gmail.com>
2021-06-27 22:01:44 +0300

    Initial commit

---
+++ lib/sound/sound_manager.dart
@@ -0,0 +1,44 @@
+import 'package:chumaki/models/company.dart';
+import 'package:flutter/services.dart';
+import 'package:soundpool/soundpool.dart';
+
+class SoundManager {
+  // do not forget to modify this map in web sounds manager implementation
+  Map<COMPANY_EVENTS, String> actionMapping = {
+    COMPANY_EVENTS.CITY_UNLOCKED: "assets/sounds/",
+  };
+
+  // contains ids of loaded sounds
+  Map<String, int> sounds = {};
+  SoundManager._internal() {}
+
+  Future initSounds() async {
+    if (sounds.isNotEmpty) {
+      return;
+    }
+    await Future.forEach(
+      actionMapping.keys,
+      ((element) async {
+        String? path = actionMapping[element];
+        if (path != null) {
+          ByteData soundData = await rootBundle.load(path);
+          int soundId;
+          soundId = await pool.load(soundData);
+          sounds[path] = soundId;
+        }
+      }),
+    );
+  }
+
+  Soundpool pool = Soundpool.fromOptions(
+      options: SoundpoolOptions(streamType: StreamType.music));
+
+  static final SoundManager instance = SoundManager._internal();
+
+  playSound(COMPANY_EVENTS action) async {
+    final soundId = sounds[actionMapping[action]];
+    if (soundId != null) {
+      await pool.play(soundId);
+    }
+  }
+}
