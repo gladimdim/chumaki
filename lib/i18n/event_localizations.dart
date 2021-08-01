191b1b75946cec6b8434c1f524b8ee8e491f3921
Dmytro Gladkyi <gladimdim@gmail.com>
2021-08-01 22:55:18 +0300

    Added Pereyaslav events.

--- lib/i18n/event_localizations.dart
+++ lib/i18n/event_localizations.dart
@@ -84,7 +84,24 @@ class EventLocalizations extends InternalLocalizations {
           "Town major decided to dry the lands near the town in order to settle more people there. For this he hired engineers from France. They need lots of hardware goods.",
       "nizhinReplaceWeaponsTitle": "Regiment Upgrade",
       "nizhinReplaceWeaponsText":
-      "Colonel Prokop Shumeiko discovered that moscovits plan to make aggressive action against Chernigiv lands. He plans to upgrade the cannons and firearms in next year. The order contains new cannons, good powder and firearms.",
+          "Colonel Prokop Shumeiko discovered that moscovits plan to make aggressive action against Chernigiv lands. He plans to upgrade the cannons and firearms in next year. The order contains new cannons, good powder and firearms.",
+
+      // Pereyaslav
+      "pereyaslavNewFerryTitle": "New Ferry",
+      "pereyaslavNewFerryText":
+          "Near Pereyaslav on Dnipro river the old ferry collapsed. Traders started to avoid this route. The castle chief ordered to build a new ferry.",
+      "pereyaslavGravesTitle": "Graves",
+      "pereyaslavGravesText":
+          "Locals keep finding gold and other artefacts near the Scythe Graves. Castle chief ordered to form an expedition. Cossacks refused to feed these lazymen that's why chief has to pay for food from his pocket.",
+      "pereyaslavBrotherhoodTitle": "Brotherhood",
+      "pereyaslavBrotherhoodText":
+          "After Azov capture, Turks were enraged and asked Poland to attack cossacks. Polish settled armed men on the Dnipro river in order to block trading routes. Pereyaslav cossacks united with Sich army and agreed to attack these outposts. But they need more money.",
+      "pereyaslavUncalmTimesTitle": "Terrible times",
+      "pereyaslavUncalmTimesText":
+          "Escalating conflict between Ukraine and Poland caused the cossacks to discuss possible military actions. They agreed to buy food and inventories in order to prepare for uncalm times.",
+      "pereyaslavHospitalTitle": "Hospital",
+      "pereyaslavHospitalText":
+          "Continuous fights in steppe causes lots of injured and sick cossacks. Pereyaslav together with Cherkasy decided to join forced and build a new hospital near the Trahtamirov monastery.",
     },
     "uk": {
       // sich
@@ -171,6 +188,22 @@ class EventLocalizations extends InternalLocalizations {
       "nizhinReplaceWeaponsTitle": "Оновлення полку",
       "nizhinReplaceWeaponsText":
           "Полковник Прокіп Шумейко дізнався про плани Московщини по нападу на Чернігівські землі, тому вирішив оновити арсенал його полку. В замовленні указані нові гармати та добрий порох з самопалами.",
+      // Pereyaslav
+      "pereyaslavNewFerryTitle": "Нова переправа",
+      "pereyaslavNewFerryText":
+          "Неподалік Переялава на Дніпрі стоїть переправа, але вона збудована ще при минулому князі, нею небезпечно пересуваться, тому всі торговці оминають її. Щоб відновити торгові шляхи, староста замку виділив кошти на побудову нової переправи.",
+      "pereyaslavGravesTitle": "Кургани",
+      "pereyaslavGravesText":
+          "Місцеві жителі постійно знаходять артефакти неподалік курганів. Староста заборонив самокопи і відряджує експедицію по розкопу всіх курганів у навколицях. Місцеві козаки відмовляються годувати пройдисвітів, тому староста закуповує харчі сам.",
+      "pereyaslavBrotherhoodTitle": "Допомога братам",
+      "pereyaslavBrotherhoodText":
+          "Після осади і взяття Азова, турки оскаженіли   і пожалілися ляхам, які посилили окупаційний контингент. Біля порогів поставили сторожі, які Січовики вирішили розбити разом з Переяславцями. Їм треба лише трошки більше зброї. ",
+      "pereyaslavUncalmTimesTitle": "Неспокійні часи",
+      "pereyaslavUncalmTimesText":
+          "Загострення у відносинах з Річ Посполитою викликає занепокоєння у козацьких верхів. На раді вирішено приготувати запаси для можливих лихих часів.",
+      "pereyaslavHospitalTitle": "Госпіталь",
+      "pereyaslavHospitalText":
+          "Постійні сутички у степах викликають збільшення калік та хворих. Спільно з Черкаським полком закладено основу Трахтемирівського монастиря-лікарні.",
     },
     "ru": {
       // Sich
@@ -258,6 +291,23 @@ class EventLocalizations extends InternalLocalizations {
       "nizhinReplaceWeaponsTitle": "Обновление полка",
       "nizhinReplaceWeaponsText":
           "Полковник Прокоп Шумейко узнал про возможные планы московитов начать агрессивные действия в Черниговских землях. по этому решил обновить вооружение в полку. В заказе указаны новые пушки и хороший порох с самопалами.",
+
+      // Pereyaslav
+      "pereyaslavNewFerryTitle": "Новая переправа",
+      "pereyaslavNewFerryText":
+          "Около Переяслава на Днепре стоит переправа построенная очень давно. Она уже разваливается и торговци обьезжают ее стороной. Чтобы восстановить торговлю в районе, староста замка выделил деньги на построение новой переправы.",
+      "pereyaslavGravesTitle": "Курганы",
+      "pereyaslavGravesText":
+          "Местные жители постояно находять артефакты около курганов. Староста запретил самокопов и отрядил свою собственную экспидицию. Козаки отказались кормить бездельников, по этому старосте нужно кормить их самим.",
+      "pereyaslavBrotherhoodTitle": "Помощь братьям",
+      "pereyaslavBrotherhoodText":
+          "После осады и взятие Азова, турки взбесились и выслали посла поляков из Порты. Поляки, испугавшись, выставили залоги на Днепре, чтобы прервать торговлю. Козаки с Сечи и Переяслава решили разбить их, но им надо побольше оружия.",
+      "pereyaslavUncalmTimesTitle": "Неспокойные времена",
+      "pereyaslavUncalmTimesText":
+          "Обострение в отношениях между Портой и Реч Посполитой вызывает обеспокоеность в козацких верхах. На раде решили приготовится к возможным смутным временам.",
+      "pereyaslavHospitalTitle": "Госпиталь",
+      "pereyaslavHospitalText":
+          "Постоянные бои в степи и на Черном море порождают множество увечных и больных. Совместно с Черкаским полком решено основать госпиталь в стенах Трахтамировского монастыря.",
     }
   };
 }
--- lib/models/cities/pereyaslav.dart
+++ lib/models/cities/pereyaslav.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:chumaki/extensions/stock.dart';
 import 'package:chumaki/models/cities/city.dart';
 import 'package:chumaki/models/cities/kyiv.dart';
+import 'package:chumaki/models/events/event.dart';
 import 'package:chumaki/models/manufacturings/manufacturing.dart';
 import 'package:chumaki/models/resources/resource.dart';
 
@@ -31,5 +32,62 @@ class Pereyaslav extends City {
             Honey(500),
             Wax(500),
           ]),
+          availableEvents: [
+            Event(
+                iconPath: "images/resources/ironore/ironore_3.png",
+                payment: Money(600),
+                requirements: [
+                  Wood(30),
+                  IronOre(50),
+                  Planks(40),
+                ],
+                localizedKey: "events.pereyaslavNewFerry",
+                artPath: "images/events/vesterfeld/river_battle_2.png"),
+            Event(
+                iconPath: "images/resources/planks/planks_3.png",
+                payment: Money(800),
+                requirements: [
+                  Bread(30),
+                  Fish(80),
+                  Salt(10),
+                ],
+                localizedKey: "events.pereyaslavGraves",
+                artPath: "images/events/vesterfeld/river_battle_2.png"),
+            Event(
+                iconPath: "images/resources/horse/horse_2.png",
+                payment: Money(1450),
+                requirements: [
+                  Horse(80),
+                  Firearm(40),
+                  Powder(50),
+                ],
+                localizedKey: "events.pereyaslavBrotherhood",
+                artPath: "images/events/vesterfeld/river_battle_2.png"),
+            Event(
+                iconPath: "images/resources/salt/salt.png",
+                payment: Money(2000),
+                requirements: [
+                  Bread(200),
+                  Grains(300),
+                  Fish(400),
+                  Salt(50),
+                  Powder(40),
+                ],
+                localizedKey: "events.pereyaslavUncalmTimes",
+                artPath: "images/events/vesterfeld/river_battle_2.png"),
+            Event(
+                iconPath: "images/resources/stone/stone_2.png",
+                payment: Money(2000),
+                requirements: [
+               Stone(200),
+                  Planks(100),
+                  Wood(100),
+                  IronOre(30),
+                  MetalParts(50),
+                ],
+                localizedKey: "events.pereyaslavHospital",
+                artPath: "images/events/vesterfeld/fortress.png"),
+
+          ],
         );
 }
