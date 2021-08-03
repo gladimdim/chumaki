8903b72cbd7aa7ed2b96717259f7ddf25bc299a3
Dmytro Gladkyi <gladimdim@gmail.com>
2021-08-03 22:54:39 +0300

    Improved logger view.

--- lib/views/logger_stats_view.dart
+++ lib/views/logger_stats_view.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 class LoggerStatsView extends StatelessWidget {
   final Logger logger;
   final VoidCallback onClose;
+
   const LoggerStatsView({Key? key, required this.logger, required this.onClose})
       : super(key: key);
 
@@ -17,7 +18,7 @@ class LoggerStatsView extends StatelessWidget {
     return DecoratedContainer3(
       child: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -30,67 +31,67 @@ class LoggerStatsView extends StatelessWidget {
             DecoratedContainer2(
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                   crossAxisAlignment: CrossAxisAlignment.end,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        children: [
-                          ClipOval(
-                            child: Image.asset(
-                              "images/wagon/cart.png",
-                              width: 64,
-                            ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      children: [
+                        ClipOval(
+                          child: Image.asset(
+                            "images/wagon/cart.png",
+                            width: 64,
                           ),
-                          TitleText(
-                              "${ChumakiLocalizations.labelWagonsBought}: ${logger.boughtWagons}"),
-                        ],
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          ClipOval(
-                            child: Image.asset(
-                              "images/leaders/leader4.png",
-                              width: 64,
-                            ),
+                        ),
+                        Text(
+                            "${ChumakiLocalizations.labelWagonsBought}: ${logger.boughtWagons}"),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ClipOval(
+                          child: Image.asset(
+                            "images/leaders/leader4.png",
+                            width: 64,
                           ),
-                          TitleText(
-                              "${ChumakiLocalizations.labelLeadersHired}: ${logger.leadersHired}"),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          ClipOval(
-                            child: Image.asset(
-                              "images/leaders/leader1.png",
-                              width: 64,
-                            ),
+                        ),
+                        Text(
+                            "${ChumakiLocalizations.labelLeadersHired}: ${logger.leadersHired}"),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        ClipOval(
+                          child: Image.asset(
+                            "images/leaders/leader1.png",
+                            width: 64,
                           ),
-                          TitleText(
-                              "${ChumakiLocalizations.labelCompletedCityEvents}: ${logger.completedCityEvents}"),
-                        ],
-                      ),
-                    ],
-                  ),
+                        ),
+                        Text(
+                            "${ChumakiLocalizations.labelCompletedCityEvents}: ${logger.completedCityEvents}"),
+                      ],
+                    ),
+                  ],
                 ),
               ),
             ),
             Text(
               ChumakiLocalizations.labelLoggerBoughtStats,
               style: Theme.of(context).textTheme.headline6,
+              textAlign: TextAlign.center,
             ),
             LoggerStockView(stock: logger.boughtStock),
             Text(
               ChumakiLocalizations.labelLoggerSoldStats,
               style: Theme.of(context).textTheme.headline6,
+              textAlign: TextAlign.center,
             ),
             LoggerStockView(stock: logger.soldStock),
             Text(
               ChumakiLocalizations.labelAchievements,
               style: Theme.of(context).textTheme.headline6,
+              textAlign: TextAlign.center,
             ),
             AchievementsProgressView(logger: logger),
           ],
