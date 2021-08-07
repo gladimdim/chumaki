d32802a27ee5cf3555329910621f89a3abe4610d
Dmytro Gladkyi <gladimdim@gmail.com>
2021-08-07 15:46:35 +0300

    returned back sound pool player.
    This fixes the issue when music was stopped by android when sound played.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chumaki/sound/sound_manager.dart';
 import 'package:chumaki/theme.dart';
 import 'package:chumaki/views/starting_view.dart';
 import 'package:flutter/material.dart';
@@ -6,6 +7,8 @@ import 'package:flutter/services.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+  await SoundManager.instance.initSounds();
   runApp(MyApp());
 }
 
