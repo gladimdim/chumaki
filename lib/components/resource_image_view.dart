a69b20fcb4b4829e99614dfb1b4f24a9e05d6b78
Dmytro Gladkyi <gladimdim@gmail.com>
2021-08-13 15:50:38 +0300

    Fixed images.

--- lib/components/resource_image_view.dart
+++ lib/components/resource_image_view.dart
@@ -1,6 +1,5 @@
 import 'package:chumaki/components/ui/bouncing_outlined_text.dart';
 import 'package:chumaki/components/ui/resized_image.dart';
-import 'package:chumaki/i18n/chumaki_localizations.dart';
 import 'package:chumaki/models/resources/resource.dart';
 import 'package:chumaki/theme.dart';
 import 'package:flutter/material.dart';
@@ -14,25 +13,22 @@ class ResourceImageView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Tooltip(
-      message: ChumakiLocalizations.getForKey(resource.fullLocalizedKey),
-      child: Stack(
-        children: [
-          ResizedImage(
-            resource.imagePath,
-            width: size.toInt(),
-          ),
-          if (showAmount)
-            Positioned(
-              bottom: 0,
-              right: 0,
-              child: BouncingOutlinedText(
-                resource.amount.toString(),
-                style: gameTextStyle,
-              ),
+    return Stack(
+      children: [
+        ResizedImage(
+          resource.imagePath,
+          width: size.toInt(),
+        ),
+        if (showAmount)
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: BouncingOutlinedText(
+              resource.amount.toString(),
+              style: gameTextStyle,
             ),
-        ],
-      ),
+          ),
+      ],
     );
   }
 }
