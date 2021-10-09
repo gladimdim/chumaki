f3aacf51ca67be4c6d507cf336d5d6f77c370d8f
Dmytro Gladkyi <gladimdim@gmail.com>
2021-10-09 08:35:32 +0300

    Added icons for web

--- copy_to_web.sh
+++ copy_to_web.sh
@@ -1,3 +1,4 @@
 flutter build web --dart-define=FLUTTER_WEB_USE_SKIA=true
 cp -r build/web/assets ../locadeserta/chumaki/
+cp -r build/web/icons ../locadeserta/chumaki/
 cp ./build/web/*.* ../locadeserta/chumaki/
